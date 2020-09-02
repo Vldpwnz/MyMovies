6c4fd8951b846cbaa725ddbf88fa61afb8b5fcbc
Vladyslav <vladislavcherkashin98@gmail.com>
2020-09-02 20:15:34 +0300

    added empty controllers

---
+++ src/main/java/vlad/project/controller/MovieController.java
@@ -0,0 +1,10 @@
+package vlad.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movies")
+public class MovieController {
+
+}
