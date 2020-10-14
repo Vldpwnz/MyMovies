efd17e11fa06a97f5c7d8245e38418cc844f227a
Vlad <vladislavcherkashin98@gmail.com>
2020-10-14 21:30:19 +0300

    added BcryptEncoding for authentication

--- src/main/java/vlad/project/security/config/UserManagmentConfig.java
+++ src/main/java/vlad/project/security/config/UserManagmentConfig.java
@@ -1,11 +1,13 @@
 package vlad.project.security.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -14,22 +16,29 @@ import org.springframework.security.provisioning.UserDetailsManager;
 @Configuration
 public class UserManagmentConfig {
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	
 	@Bean
 	public UserDetailsService userDetailsService() {
+		
+		String password = passwordEncoder.encode("password");
 		
 		UserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
 		
 		UserDetails user = User.withUsername("vlad")
-				.password("password")
+				.password(password)
 				.authorities("read")
 				.build();
+
 		userDetailsService.createUser(user);
 		return userDetailsService;
 	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 	
 	
