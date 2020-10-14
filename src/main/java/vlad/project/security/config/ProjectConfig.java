86d7c0285d708a64116933dbb589a0d3202e4f5d
Vlad <vladislavcherkashin98@gmail.com>
2020-10-14 20:30:40 +0300

    removed second configuration

--- src/main/java/vlad/project/security/config/ProjectConfig.java
+++ src/main/java/vlad/project/security/config/ProjectConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -16,6 +17,13 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) {
 		auth.authenticationProvider(authProvider);
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeRequests()
+			.anyRequest().authenticated();
 	}
 	
 	
